153cd1d5024a73b51e8db965ea274d07a8d94a4c
Husniya Umrbekova <husniyaxonumrbekova@gmail.com>
2026-03-25 16:13:39 +0500

    amaliyot
    mmmm

---
+++ amaliyat_ms.py
@@ -0,0 +1,26 @@
+
+
+# import math as m
+
+# ozgaruvchi =m.cos(0.2)
+# print(ozgaruvchi)
+
+# import masalalar    
+# ozgaruvchi = masalalar.ayirma(45, 25)
+# print(ozgaruvchi) 
+
+
+# import masalalar
+# ozgaruvchi = masalalar.kvadrat(5)
+# print(ozgaruvchi)
+
+# import function_dict as fuc
+# daaeja=fuc.ayirma(45, 25)
+# print(daaeja)
+
+
+
+# from function_dict import ayirma,yigindi
+# daraja=ayirma(45, 25)
+# daraja1=yigindi(4, 5)
+# print(daraja)
